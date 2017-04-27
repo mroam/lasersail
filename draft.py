82c455e47c15df620aab9f3cc7efaf08eb66de16
mroam <mroam@saintannsny.org>
2017-04-27 11:54:39 -0400

    Update draft.py
    specified approx units for all terms

--- draft.py
+++ draft.py
@@ -3,24 +3,29 @@ import math
 #Constants:
 c = 299792458 #m/s
 laserFrequency = input('Enter a laser freqeuncy between 400-3*10^16 Hz:')
-laserPower = input('Enter a laser power:')
-sailArea = input('Enter a size for the sail:')
+laserPower = input('Enter a laser power:') # watts = ( newton * m) /sec  # or calculate power = k * (amp^2) * freq
+# note: "intensity" is power/area of the spread out laser
+sailArea = input('Enter a size for the sail:')  # m^2
 thrusterFuel = input('Enter amount of thruster fuel: ')
-wSpot = 
+wSpot = #we might be able to get rid of this
+# 
 laserWavelength = c/laserFrequency
-laserDivergence = laserWavelength/(pi * wSpot)
-distanceFromSol = 
-equilibriumTemp = 
+laserDivergence = laserWavelength/(pi * wSpot) #angle # might just need some constant instead of pi*wSPOT
+distanceFromSol =     # m
+equilibriumTemp =       # celsius
 #Mylar specs:
 mylarDensity = 1.39 #g/cm^3
 meltingPoint = 254 #Celsius
 tensileStrength = 24 #kg/mm^2
-forceApplied =                ##Will be a function of laserFrequency, laserPower, and distanceFromSol
-sailMass =                    ##Will be a function of area
-totalMass = thrusterFuel + sailMass
-critMass =                    ##Will be the maximum mass the sail can have to be pushed by light. WIll find later
-sailAccel = forceApplied * totalMass ##Acceleration of sail
-finalV = (2*sailAccel*distanceFromSol)^1/2 ##Final velocity after laser stops firing
+
+forceApplied =         #newtons = kg m/sec/sec    
+##Will be a function of laserPower (- waste including heat), laserDivergence, and distanceFromSol
+
+sailMass =          #kg          ##Will be a function of area
+totalMass = thrusterFuel + sailMass  #kg
+critMass =       #kg             ##Will be the maximum mass the sail can have to be pushed by light. WIll find later
+sailAccel = forceApplied * totalMass  # m/sec/sec ###Acceleration of sail
+finalV = (2*sailAccel*distanceFromSol)^1/2  # m/sec  ##Final velocity after laser stops firing
 if totalMass > critMass:
     print "You are too heavy!"
 
